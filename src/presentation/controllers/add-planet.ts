f4f76810d3aa542939af312b259f7e1e60860fb3
eduviictor <eduvictornobrega@gmail.com>
2021-03-03 20:51:02 -0300

    feat: ensure AddPlanetController returns 400 if no climate is provided

--- src/presentation/controllers/add-planet.ts
+++ src/presentation/controllers/add-planet.ts
@@ -1,14 +1,19 @@
-import { MissingParamError } from "../errors/missing-param-error";
-import { badRequest } from "../helpers/http";
-import { Controller } from "../protocols/controller";
-import { HttpResponse, HttpRequest } from "../protocols/http";
+import { MissingParamError } from '../errors/missing-param-error';
+import { badRequest } from '../helpers/http';
+import { Controller } from '../protocols/controller';
+import { HttpResponse, HttpRequest } from '../protocols/http';
 
 export class AddPlanetController implements Controller {
   constructor() {}
 
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    if (!request.body.name) {
-      return badRequest(new MissingParamError("name"));
+    const requiredFields = ['name', 'climate'];
+
+    for (const field of requiredFields) {
+      if (!request.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
+    return null;
   }
 }
