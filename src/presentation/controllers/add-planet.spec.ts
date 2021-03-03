901d700b4e99fd89730c89ef29f6b353a935709b
eduviictor <eduvictornobrega@gmail.com>
2021-03-03 20:51:16 -0300

    test: ensure AddPlanetController returns 400 if no climate is provided

--- src/presentation/controllers/add-planet.spec.ts
+++ src/presentation/controllers/add-planet.spec.ts
@@ -1,23 +1,37 @@
-import { MissingParamError } from "@/presentation/errors/missing-param-error";
-import { AddPlanetController } from "@/presentation/controllers/add-planet";
-import { AddPlanet } from "@/domain/usecases/add-planet";
+import { MissingParamError } from '@/presentation/errors/missing-param-error';
+import { AddPlanetController } from '@/presentation/controllers/add-planet';
+import { AddPlanet } from '@/domain/usecases/add-planet';
 
 const makeSut = (): AddPlanetController => {
   return new AddPlanetController();
 };
 
-describe("AddPlanet Controller", () => {
-  test("Should return 400 if no name is provided", async () => {
+describe('AddPlanet Controller', () => {
+  test('Should return 400 if no name is provided', async () => {
     const sut = makeSut();
     const httpRequest = {
       body: {
-        climate: "any_climate",
-        ground: "any_ground"
-      }
+        climate: 'any_climate',
+        ground: 'any_ground',
+      },
     };
     const httpResponse = await sut.handle(httpRequest);
     console.log(httpResponse);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new MissingParamError("name"));
+    expect(httpResponse.body).toEqual(new MissingParamError('name'));
+  });
+
+  test('Should return 400 if no climate is provided', async () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        ground: 'any_ground',
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    console.log(httpResponse);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('climate'));
   });
 });
