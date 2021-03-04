efa16cf3d47b184d964394d831ddcf6fe454acf7
eduviictor <eduvictornobrega@gmail.com>
2021-03-04 00:41:53 -0300

    test: ensure SwapiAdapter find the planet on the first request

--- src/infra/request/swapi-adapter.spec.ts
+++ src/infra/request/swapi-adapter.spec.ts
@@ -1,17 +1,20 @@
 import { ApiRequest } from '../protocols/api-request';
+import { SwapiResponse } from '../protocols/swapi-response';
 import { SwapiAdapter } from './swapi-adapter';
 import { SwapiConfig } from './swapi-config';
 
 const makeApiRequest = (): ApiRequest => {
   class ApiRequestStub implements ApiRequest {
     async get(url: string, headers?: any): Promise<any> {
-      const fakeResponse = {
+      const fakeResponse: SwapiResponse = {
         results: [
           {
             name: 'any_name',
+            films: ['any_film', 'any_film'],
           },
         ],
         next: 'any_next_page',
+        previous: null,
       };
       return await new Promise((resolve) => resolve(fakeResponse));
     }
@@ -38,12 +41,18 @@ describe('Swapi Adapter', () => {
     const { sut, apiRequestStub } = makeSut();
     const apiRequestSpy = jest.spyOn(apiRequestStub, 'get');
 
-    await sut.getMoviesPlanet('any_name');
+    await sut.getMoviesPlanet('any_planet');
     const expectedUrl = `${SwapiConfig.url}/planets`;
 
     expect(apiRequestSpy).toHaveBeenCalledWith(
       expectedUrl,
       SwapiConfig.headers
     );
+  });
+
+  test('Should find the planet on the first request', async () => {
+    const { sut } = makeSut();
+    const numberMovies = await sut.getMoviesPlanet('any_name');
+    expect(numberMovies).toBe(2);
   });
 });
