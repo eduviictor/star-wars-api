dc4d9d6152d7f31bcb21fce7ecef97d3cc1ef559
eduviictor <eduvictornobrega@gmail.com>
2021-03-03 22:50:48 -0300

    feat: ensure AddPlanetController returns 500 if AddPlanet throws

--- src/presentation/controllers/add-planet.ts
+++ src/presentation/controllers/add-planet.ts
@@ -1,6 +1,7 @@
 import { AddPlanet } from '@/domain/usecases/add-planet';
 import { MissingParamError } from '../errors/missing-param-error';
-import { badRequest } from '../helpers/http';
+import { ServerError } from '../errors/server-error';
+import { badRequest, serverError } from '../helpers/http';
 import { Controller } from '../protocols/controller';
 import { HttpResponse, HttpRequest } from '../protocols/http';
 
@@ -8,15 +9,20 @@ export class AddPlanetController implements Controller {
   constructor(private readonly addPlanet: AddPlanet) {}
 
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['name', 'climate', 'ground'];
+    try {
+      const requiredFields = ['name', 'climate', 'ground'];
 
-    for (const field of requiredFields) {
-      if (!request.body[field]) {
-        return badRequest(new MissingParamError(field));
+      for (const field of requiredFields) {
+        if (!request.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
-    const { name, climate, ground } = request.body;
+      const { name, climate, ground } = request.body;
 
-    await this.addPlanet.add({ name, climate, ground });
+      await this.addPlanet.add({ name, climate, ground });
+    } catch (error) {
+      console.error(error);
+      return serverError();
+    }
   }
 }
