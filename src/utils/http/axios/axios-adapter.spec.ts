effc3a470e7f8d85ab1f51db16ebcc35e3cd85c8
eduviictor <eduvictornobrega@gmail.com>
2021-03-09 22:01:15 -0300

    test: ensure AxiosAdapter call axios with correct values and return data on success

---
+++ src/utils/http/axios/axios-adapter.spec.ts
@@ -0,0 +1,43 @@
+import { AxiosAdapter } from './axios-adapter';
+import axios from 'axios';
+
+jest.mock('axios', () => ({
+  async get(): Promise<any> {
+    return await new Promise((resolve) =>
+      resolve({
+        data: 'success',
+      })
+    );
+  },
+}));
+
+const makeSut = (): AxiosAdapter => new AxiosAdapter();
+
+describe('Axios Adapter', () => {
+  test('Should call axios with correct values', async () => {
+    const sut = makeSut();
+    const axiosSpy = jest.spyOn(axios, 'get');
+
+    const url = 'any_url';
+    const headers = {
+      Authorization: 'any_authorization',
+    };
+
+    await sut.get(url, headers);
+
+    expect(axiosSpy).toHaveBeenCalledWith(url, { headers });
+  });
+
+  test('Should return data on success', async () => {
+    const sut = makeSut();
+
+    const url = 'any_url';
+    const headers = {
+      Authorization: 'any_authorization',
+    };
+
+    const data = await sut.get(url, headers);
+
+    expect(data).toBe('success');
+  });
+});
