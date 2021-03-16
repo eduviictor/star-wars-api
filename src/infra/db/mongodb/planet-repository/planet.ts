5c950ff48148c9f41d264d45d8cf8c8b54f0119b
eduviictor <eduvictornobrega@gmail.com>
2021-03-16 09:36:32 -0300

    feat: ensure PlanetMongoRepository return a list of planets successfully

--- src/infra/db/mongodb/planet-repository/planet.ts
+++ src/infra/db/mongodb/planet-repository/planet.ts
@@ -1,12 +1,20 @@
 import { PlanetModel } from '@/domain/models/planet';
 import { AddPlanetModelDatabase } from '@/domain/usecases/add-planet';
 import { AddPlanetRepository } from '@/services/protocols/db/add-planet-repository';
+import { GetPlanetsRepository } from '@/services/protocols/db/db-get-planets-repository';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class PlanetMongoRepository implements AddPlanetRepository {
+export class PlanetMongoRepository
+  implements AddPlanetRepository, GetPlanetsRepository {
   async add(planetData: AddPlanetModelDatabase): Promise<PlanetModel> {
     const planetCollection = await MongoHelper.getCollection('planets');
     const result = await planetCollection.insertOne(planetData);
     return MongoHelper.map(result.ops[0]);
+  }
+
+  async index(): Promise<PlanetModel[]> {
+    const planetCollection = await MongoHelper.getCollection('planets');
+    const result = await planetCollection.find().toArray();
+    return MongoHelper.mapCollection(result);
   }
 }
