918d6bd721889f9c82eee9728ffaf7d99db60726
eduviictor <eduvictornobrega@gmail.com>
2021-03-16 09:36:37 -0300

    test: ensure PlanetMongoRepository return a list of planets successfully

--- src/infra/db/mongodb/planet-repository/planet.spec.ts
+++ src/infra/db/mongodb/planet-repository/planet.spec.ts
@@ -19,21 +19,46 @@ describe('Planet Mongo Repository', () => {
     return new PlanetMongoRepository();
   };
 
-  test('Should return an planet on success', async () => {
-    const sut = makeSut();
+  describe('add()', () => {
+    test('Should return an planet on success', async () => {
+      const sut = makeSut();
 
-    const planet = await sut.add({
-      name: 'any_name',
-      climate: 'any_climate',
-      ground: 'any_ground',
-      movies: 5,
+      const planet = await sut.add({
+        name: 'any_name',
+        climate: 'any_climate',
+        ground: 'any_ground',
+        movies: 5,
+      });
+
+      expect(planet).toBeTruthy();
+      expect(planet.id).toBeTruthy();
+      expect(planet.name).toBe('any_name');
+      expect(planet.climate).toBe('any_climate');
+      expect(planet.ground).toBe('any_ground');
+      expect(planet.movies).toBe(5);
     });
+  });
 
-    expect(planet).toBeTruthy();
-    expect(planet.id).toBeTruthy();
-    expect(planet.name).toBe('any_name');
-    expect(planet.climate).toBe('any_climate');
-    expect(planet.ground).toBe('any_ground');
-    expect(planet.movies).toBe(5);
+  describe('index()', () => {
+    test('Should return a list of planets successfully', async () => {
+      const sut = makeSut();
+
+      await sut.add({
+        name: 'any_name',
+        climate: 'any_climate',
+        ground: 'any_ground',
+        movies: 5,
+      });
+
+      const planets = await sut.index();
+
+      expect(planets).toBeTruthy();
+      expect(Array.isArray(planets)).toBe(true);
+      expect(planets[0].id).toBeTruthy();
+      expect(planets[0].name).toBe('any_name');
+      expect(planets[0].climate).toBe('any_climate');
+      expect(planets[0].ground).toBe('any_ground');
+      expect(planets[0].movies).toBe(5);
+    });
   });
 });
