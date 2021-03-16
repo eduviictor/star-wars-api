c877288e9d4c17c79e97c61616fa7751b041188e
eduviictor <eduvictornobrega@gmail.com>
2021-03-16 15:33:04 -0300

    feat: ensure getByName return an planet with success

--- src/infra/db/mongodb/planet-repository/planet.ts
+++ src/infra/db/mongodb/planet-repository/planet.ts
@@ -1,11 +1,15 @@
 import { PlanetModel } from '@/domain/models/planet';
 import { AddPlanetModelDatabase } from '@/domain/usecases/add-planet';
 import { AddPlanetRepository } from '@/services/protocols/db/db-add-planet-repository';
+import { GetPlanetsByNameRepository } from '@/services/protocols/db/db-get-planets-by-name-repository';
 import { GetPlanetsRepository } from '@/services/protocols/db/db-get-planets-repository';
 import { MongoHelper } from '../helpers/mongo-helper';
 
 export class PlanetMongoRepository
-  implements AddPlanetRepository, GetPlanetsRepository {
+  implements
+    AddPlanetRepository,
+    GetPlanetsRepository,
+    GetPlanetsByNameRepository {
   async add(planetData: AddPlanetModelDatabase): Promise<PlanetModel> {
     const planetCollection = await MongoHelper.getCollection('planets');
     const result = await planetCollection.insertOne(planetData);
@@ -16,5 +20,16 @@ export class PlanetMongoRepository
     const planetCollection = await MongoHelper.getCollection('planets');
     const result = await planetCollection.find().toArray();
     return MongoHelper.mapCollection(result);
+  }
+
+  async getByName(name: string): Promise<PlanetModel> {
+    const planetCollection = await MongoHelper.getCollection('planets');
+    const result = await planetCollection.findOne({
+      name: name,
+    });
+    if (!result) {
+      return null;
+    }
+    return MongoHelper.map(result);
   }
 }
