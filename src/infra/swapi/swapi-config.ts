bcad93e968efafeed91a194b3b74998854ad7f42
eduviictor <eduvictornobrega@gmail.com>
2021-03-16 10:31:06 -0300

    fix: add url default to swapi

--- src/infra/swapi/swapi-config.ts
+++ src/infra/swapi/swapi-config.ts
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 export const SwapiConfig = {
-  url: process.env.SWAPI_URL,
-  headers: {}
+  url: process.env.SWAPI_URL || 'https://swapi.dev/api',
+  headers: {},
 };
