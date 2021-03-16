20235cbae7ad480126ff2e46e4d7345644395ef2
eduviictor <eduvictornobrega@gmail.com>
2021-03-16 15:54:42 -0300

    test: ensure GetPlanetsByIdController throw if GetPlanetsById throws

---
+++ src/presentation/controllers/get-planets-by-id.spec.ts
@@ -0,0 +1,54 @@
+import { PlanetModel } from '@/domain/models/planet';
+import { GetPlanetsById } from '@/domain/usecases/get-planets-by-id';
+import { MissingParamError } from '../errors/missing-param-error';
+import { ServerError } from '../errors/server-error';
+import { GetPlanetsByIdController } from './get-planets-by-id';
+
+const makeGetPlanetsById = (): GetPlanetsById => {
+  class GetPlanetsByIdStub implements GetPlanetsById {
+    async getById(id: string): Promise<PlanetModel> {
+      const fakePlanet = {
+        id: 'valid_id',
+        name: 'valid_name',
+        climate: 'valid_climate',
+        ground: 'valid_ground',
+        movies: 5,
+      };
+
+      return await new Promise((resolve) => resolve(fakePlanet));
+    }
+  }
+  return new GetPlanetsByIdStub();
+};
+
+interface SutTypes {
+  sut: GetPlanetsByIdController;
+  getPlanetsByIdStub: GetPlanetsById;
+}
+
+const makeSut = (): SutTypes => {
+  const getPlanetsByIdStub = makeGetPlanetsById();
+  const sut = new GetPlanetsByIdController(getPlanetsByIdStub);
+  return {
+    sut,
+    getPlanetsByIdStub,
+  };
+};
+
+describe('GetPlanetsById Controller', () => {
+  test('Should return 500 if GetPlanetsById throws', async () => {
+    const { sut, getPlanetsByIdStub } = makeSut();
+    jest
+      .spyOn(getPlanetsByIdStub, 'getById')
+      .mockImplementationOnce(async () => {
+        return await new Promise((resolve, reject) => reject(new Error()));
+      });
+    const httpRequest = {
+      body: {},
+      params: { id: 'any_name' },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+  });
+});
