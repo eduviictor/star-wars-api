39868be36f4c6ccac6ce9618f1336f85810b083b
eduviictor <eduvictornobrega@gmail.com>
2021-03-16 16:04:02 -0300

    test: ensure DbGetPlanetsById call GetPlanetsByIdRepository

---
+++ src/services/usecases/db-get-planets-by-id.spec.ts
@@ -0,0 +1,45 @@
+import { PlanetModel } from '@/domain/models/planet';
+import { DbGetPlanetsById } from './db-get-planets-by-id';
+import { GetPlanetsByIdRepository } from '../protocols/db/db-get-planets-by-id-repository';
+
+const makeGetPlanetsByIdRepository = (): GetPlanetsByIdRepository => {
+  class GetPlanetsByIdRepositoryStub implements GetPlanetsByIdRepository {
+    async getById(): Promise<PlanetModel> {
+      const fakePlanet = {
+        id: 'valid_id',
+        name: 'valid_name',
+        climate: 'valid_climate',
+        ground: 'valid_ground',
+        movies: 5,
+      };
+      return await new Promise((resolve) => resolve(fakePlanet));
+    }
+  }
+
+  return new GetPlanetsByIdRepositoryStub();
+};
+
+interface SutTypes {
+  sut: DbGetPlanetsById;
+  getPlanetsByIdRepositoryStub: GetPlanetsByIdRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const getPlanetsByIdRepositoryStub = makeGetPlanetsByIdRepository();
+  const sut = new DbGetPlanetsById(getPlanetsByIdRepositoryStub);
+  return {
+    sut,
+    getPlanetsByIdRepositoryStub,
+  };
+};
+
+describe('DbGetPlanetsById Usecase', () => {
+  test('Should call GetPlanetsByIdRepository', async () => {
+    const { sut, getPlanetsByIdRepositoryStub } = makeSut();
+    const indexSpy = jest.spyOn(getPlanetsByIdRepositoryStub, 'getById');
+
+    await sut.getById('any_id');
+
+    expect(indexSpy).toHaveBeenCalledTimes(1);
+  });
+});
