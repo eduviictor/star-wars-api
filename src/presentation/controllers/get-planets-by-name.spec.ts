a48c552b8b834438ad67afb712bb016d768425f4
eduviictor <eduvictornobrega@gmail.com>
2021-03-16 14:53:59 -0300

    test: ensure GetPlanetByNameController throw if GetPlanetsByName throws

---
+++ src/presentation/controllers/get-planets-by-name.spec.ts
@@ -0,0 +1,53 @@
+import { PlanetModel } from '@/domain/models/planet';
+import { GetPlanetsByName } from '@/domain/usecases/get-planets-by-name';
+import { ServerError } from '../errors/server-error';
+import { GetPlanetsByNameController } from './get-planets-by-name';
+
+const makeGetPlanetsByName = (): GetPlanetsByName => {
+  class GetPlanetsByNameStub implements GetPlanetsByName {
+    async getByName(name: string): Promise<PlanetModel> {
+      const fakePlanet = {
+        id: 'valid_id',
+        name: 'valid_name',
+        climate: 'valid_climate',
+        ground: 'valid_ground',
+        movies: 5,
+      };
+
+      return await new Promise((resolve) => resolve(fakePlanet));
+    }
+  }
+  return new GetPlanetsByNameStub();
+};
+
+interface SutTypes {
+  sut: GetPlanetsByNameController;
+  getPlanetsByNameStub: GetPlanetsByName;
+}
+
+const makeSut = (): SutTypes => {
+  const getPlanetsByNameStub = makeGetPlanetsByName();
+  const sut = new GetPlanetsByNameController(getPlanetsByNameStub);
+  return {
+    sut,
+    getPlanetsByNameStub,
+  };
+};
+
+describe('GetPlanetsByName Controller', () => {
+  test('Should return 500 if GetPlanetsByName throws', async () => {
+    const { sut, getPlanetsByNameStub } = makeSut();
+    jest
+      .spyOn(getPlanetsByNameStub, 'getByName')
+      .mockImplementationOnce(async () => {
+        return await new Promise((resolve, reject) => reject(new Error()));
+      });
+    const httpRequest = {
+      body: {},
+      params: { name: 'any_name' },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+  });
+});
