9a074a91bed41ded85d0960013d4bedde8678166
eduviictor <eduvictornobrega@gmail.com>
2021-03-18 00:40:49 -0300

    test: ensure DbDeletePlanets call DeletePlanetsRepository

---
+++ src/services/usecases/db-delete-planets.spec.ts
@@ -0,0 +1,37 @@
+import { DeletePlanetsRepository } from '../protocols/db/db-delete-planets-repository';
+import { DbDeletePlanets } from './db-delete-planets';
+
+const makeDeletePlanetsRepository = (): DeletePlanetsRepository => {
+  class DeletePlanetsRepositoryStub implements DeletePlanetsRepository {
+    async delete(): Promise<void> {
+      return await new Promise((resolve) => resolve());
+    }
+  }
+
+  return new DeletePlanetsRepositoryStub();
+};
+
+interface SutTypes {
+  sut: DbDeletePlanets;
+  deletePlanetsRepositoryStub: DeletePlanetsRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const deletePlanetsRepositoryStub = makeDeletePlanetsRepository();
+  const sut = new DbDeletePlanets(deletePlanetsRepositoryStub);
+  return {
+    sut,
+    deletePlanetsRepositoryStub,
+  };
+};
+
+describe('DbDeletePlanets Usecase', () => {
+  test('Should call DeletePlanetsRepository', async () => {
+    const { sut, deletePlanetsRepositoryStub } = makeSut();
+    const deleteSpy = jest.spyOn(deletePlanetsRepositoryStub, 'delete');
+
+    await sut.delete('any_id');
+
+    expect(deleteSpy).toHaveBeenCalledTimes(1);
+  });
+});
